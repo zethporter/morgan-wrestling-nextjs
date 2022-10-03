49d638efb7e4ce5adbf96c40da75cfc750e727b0
Zeth Porter <zeth@pop-os.localdomain>
2022-10-03 12:43:12 -0600

    Added tailwind to project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,txs}"
+  ],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
+      },
+    },
+  },
+  plugins: [],
+}
