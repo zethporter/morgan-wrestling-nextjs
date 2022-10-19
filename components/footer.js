c8f85bf13d522b522ef9dfad7f38e2b580af74c4
zethporter <zethieboy@gmail.com>
2022-10-18 23:51:56 -0600

    changed a tag to Link

--- components/footer.js
+++ components/footer.js
@@ -1,12 +1,7 @@
-import {
-  Box,
-  Typography,
-  IconButton,
-  Grid,
-  Stack
-} from '@mui/material';
+
 import { SiFacebook, SiGuilded, SiInstagram } from 'react-icons/si'
 import { openInNewTab, openInSameTab } from './utils';
+import Link from 'next/link'
 
 const Footer = () => {
   return (
@@ -14,7 +9,7 @@ const Footer = () => {
       <div className="bg-transparent mx-auto p-2 flex flex-row justify-center space-x-3">
         <SiFacebook onClick={() => openInNewTab("https://www.facebook.com/groups/1422093051346115/")} className="fill-gold-600 hover:fill-gold-400 cursor-pointer" />
         <SiInstagram onClick={() => openInNewTab("https://www.instagram.com/morgantrojanwrestling/")} className="fill-gold-600 hover:fill-gold-400 cursor-pointer" />
-        <a href="/team-communication"><SiGuilded className="fill-gold-600 hover:fill-gold-400 cursor-pointer" /></a>
+        <Link href="/team-communication"><SiGuilded className="fill-gold-600 hover:fill-gold-400 cursor-pointer" /></Link>
       </div>
     </div>
   )
